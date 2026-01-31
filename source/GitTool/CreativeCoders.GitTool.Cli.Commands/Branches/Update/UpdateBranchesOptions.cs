c85c9ca4517482119990ef1544e46942ef56f75e
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-01-31 17:09:56 +0100

    Add `UpdateBranchesCommand` to update local permanent branches
    - Implement `UpdateBranchesCommand` to pull updates for permanent local branches (e.g., production, main, develop) from remote.
    - Add `UpdateBranchesOptions` to include options like `SkipFetchPrune`.
    - Update project configuration to include the new command.

---
+++ source/GitTool/CreativeCoders.GitTool.Cli.Commands/Branches/Update/UpdateBranchesOptions.cs
@@ -0,0 +1,8 @@
+﻿using JetBrains.Annotations;
+
+namespace CreativeCoders.GitTool.Cli.Commands.Branches.Update;
+
+public class UpdateBranchesOptions
+{
+    [UsedImplicitly] public bool SkipFetchPrune { get; set; }
+}
