ca794b0dafab6cb8edd0c4e16825f47f79e6ddee
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-02-15 10:45:35 +0100

    Update `VersionFormatException` message for clearer description of valid version formats

--- source/GitTool/CreativeCoders.GitTool.Base/Versioning/VersionFormatException.cs
+++ source/GitTool/CreativeCoders.GitTool.Base/Versioning/VersionFormatException.cs
@@ -5,7 +5,7 @@ namespace CreativeCoders.GitTool.Base.Versioning;
 
 [PublicAPI]
 public class VersionFormatException(string? version, string? message = null) : Exception(message ??
-    $"The version '{version}' has an invalid format. The version must consist of at least 3 parts separated by dots (e.g. '1.0.0').")
+    $"The version '{version}' has an invalid format. The version must consist of 1 to 3 numeric parts separated by dots (e.g. '1.0.0').")
 {
     public string? Version { get; } = version;
 }
