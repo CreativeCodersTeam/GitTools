3c05a0977684a9f523035f26571862a22e75a7b1
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-03-29 12:09:33 +0200

    refactor: update default parameter value to `null` in `IGcmCoreCredentialStore.Create`

--- source/Git/CreativeCoders.Git.Auth.CredentialManagerCore/IGcmCoreCredentialStore.cs
+++ source/Git/CreativeCoders.Git.Auth.CredentialManagerCore/IGcmCoreCredentialStore.cs
@@ -13,5 +13,5 @@ public interface IGcmCoreCredentialStore
     /// <param name="credentialsNameSpace">The namespace used to scope credentials within the credential store.
     /// Pass <see langword="null"/> to use the default namespace.</param>
     /// <returns>An <see cref="ICredentialStore"/> scoped to the given <paramref name="credentialsNameSpace"/>.</returns>
-    ICredentialStore Create(string? credentialsNameSpace = default);
+    ICredentialStore Create(string? credentialsNameSpace = null);
 }
